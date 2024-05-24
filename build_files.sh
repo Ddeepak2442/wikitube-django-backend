5bc5215cdd6ce3f7604ee5df77fce2ceee5589c3
unknown <deepaksaivenkat1111@gmail.com>
2024-05-24 17:46:07 +0530

    settings file fixed

--- build_files.sh
+++ build_files.sh
@@ -1,14 +1,20 @@
-# build_files.sh
-
 #!/bin/bash
+
 # Ensure pip is available
 /usr/bin/python3.9 -m ensurepip --upgrade
+
 # Upgrade pip
 /usr/bin/python3.9 -m pip install --upgrade pip
+
 # Install dependencies from requirements.txt
 /usr/bin/python3.9 -m pip install -r requirements.txt
-# Add other necessary build steps below
+
+# Create static directory if it doesn't exist
+mkdir -p /vercel/path0/static
+
+# Collect static files
 python3.9 manage.py collectstatic --noinput
 
+# Apply database migrations
 python3.9 manage.py makemigrations
 python3.9 manage.py migrate
