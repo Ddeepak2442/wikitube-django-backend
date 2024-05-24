a151e1daf45e615644d2a62151d231ffbba928f3
unknown <deepaksaivenkat1111@gmail.com>
2024-05-24 10:45:28 +0530

    first commit

---
+++ build_files.sh
@@ -0,0 +1,14 @@
+# build_files.sh
+
+#!/bin/bash
+# Ensure pip is available
+/usr/bin/python3.9 -m ensurepip --upgrade
+# Upgrade pip
+/usr/bin/python3.9 -m pip install --upgrade pip
+# Install dependencies from requirements.txt
+/usr/bin/python3.9 -m pip install -r requirements.txt
+# Add other necessary build steps below
+python3.9 manage.py collectstatic --noinput
+
+python3.9 manage.py makemigrations
+python3.9 manage.py migrate
