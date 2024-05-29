49f42aecf0d81ebc4df457b52aeaa3d7ed9c715d
KATARI REVATHI <katari.revathi@gmail.com>
2024-05-29 23:45:46 +0530

    first

--- backend/urls.py
+++ backend/urls.py
@@ -20,7 +20,7 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenVerifyView
 
 urlpatterns = [
-    path('/', admin.site.urls),
+    path('', admin.site.urls),
     path('api/', include('account.urls')),
     path('api/token/', TokenObtainPairView.as_view()),
     path('api/token/verify/', TokenVerifyView.as_view())
